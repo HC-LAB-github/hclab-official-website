8187f6f580744473214df0683174e15afe0957e0
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-09-26 21:58:52 +0800

    refactor: temporary file

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Index() {
   return (
     <main>
-      <h1 className="text-6xl">Hello Home</h1>
+      <h1 className="text-6xl">Hello HCLAB</h1>
     </main>
   );
 }
